20ab47086c8adf2d8a0ac357916dcc58320bb256
Amr <sheeka26122000@gmail.com>
2023-03-15 13:34:31 +0200

    code arc

--- components/screens/Chat.jsx
+++ components/screens/Chat.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
-
-const ChatCard = ({ chat }) => {
-  return (
-    <View style={styles.card}>
-      <Image source={chat.photo} style={styles.cardPhoto} />
-      <View style={styles.cardContent}>
-        <Text style={styles.cardTitle}>{chat.name}</Text>
-        <Text style={styles.cardMessage}>{chat.message}</Text>
-      </View>
-    </View>
-  );
-};
-
+import chats from '../consts/Chats';
+import ChatCard from '../subcomponents/ChatCard'
 const Chat= () => {
   const renderChat = ({ item }) => <ChatCard chat={item} />;
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -48,38 +36,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#fff',
   },
-  card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    marginBottom: 20,
-  },
-  cardPhoto: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    margin: 10,
-  },
-  cardContent: {
-    flex: 1,
-  },
-  cardTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  cardMessage: {
-    fontSize: 14,
-    color: '#555',
-  },
+  
 });
 
 export default Chat;
