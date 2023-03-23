f23ac92f121b6630edc14ef8a67219a15e035630
Amr <sheeka26122000@gmail.com>
2023-03-24 00:03:56 +0200

    تا

--- components/screens/Home.jsx
+++ components/screens/Home.jsx
@@ -10,13 +10,21 @@ const Home = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.heading}>Chats</Text>
+        <View style={styles.Title}>
+          <Text style={styles.heading}>All doctors treat,but a good doctor lets nature heal.</Text>
+        </View>
       </View>
-      <FlatList
-        data={doctors}
-        renderItem={renderChat}
-        keyExtractor={(item) => item.id.toString()}
-      />
+      <View style={styles.list}>
+        <View style={styles.round}>
+          <FlatList
+            data={doctors}
+            renderItem={renderChat}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
+      
+      </View>
+      
     </View>
   );
 };
@@ -24,12 +32,12 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor:"#288771",
   },
   header: {
     backgroundColor: "#288771",
     width: "100%",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "center",
     paddingVertical: 30,
     marginBottom: 20,
@@ -38,6 +46,19 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "#fff",
+  },
+  Title:{
+    width:260,
+    alignItems:"flex-start",
+    paddingHorizontal:20,
+  },
+  list:{
+    borderTopLeftRadius:25,
+    borderTopRightRadius:25,
+    backgroundColor:"#fff",
+  },
+  round:{
+    marginTop:20,
   },
 });
 
