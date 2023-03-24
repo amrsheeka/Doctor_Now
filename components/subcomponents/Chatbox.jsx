c9ebe8df67e0ea31dc26333f888072fd8b647516
abdo <201927036@std.sci.cu.edu.eg>
2023-03-24 19:11:48 +0200

    chat

--- components/subcomponents/Chatbox.jsx
+++ components/subcomponents/Chatbox.jsx
@@ -1,47 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 
-
-const Chatbox = ({ navigation, route }) => {
+export default function Chatbox() {
+  const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
+  const [isSender, setIsSender] = useState(true);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello!',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
-  }, []);
+  const handleSendMessage = () => {
+    const newMessage = {
+      message,
+      date: new Date(),
+      isSender
+    };
 
-  function onSend(newMessages = []) {
-    setMessages(GiftedChat.append(messages, newMessages));
+    setMessages([...messages, newMessage]);
+    setIsSender(!isSender);
+    setMessage('');
   }
 
+  const renderMessage = ({ item }) => (
+    <View style={[
+      styles.messageContainer,
+      item.isSender ? styles.senderMessageContainer : styles.receiverMessageContainer
+    ]}>
+      <Text style={[
+        styles.message,
+        item.isSender ? styles.senderMessage : styles.receiverMessage
+      ]}>{item.message}</Text>
+      <Text style={styles.date}>{item.date.toLocaleString()}</Text>
+    </View>
+  );
+
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={onSend}
-      user={{
-        _id: 1,
-      }}
-    />
+    <View style={styles.container}>
+      <FlatList
+        data={messages}
+        renderItem={renderMessage}
+        keyExtractor={(item, index) => index.toString()}
+        style={styles.messages}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={isSender ? "Type a message (you)" : "Type a message (receiver)"}
+          value={message}
+          onChangeText={text => setMessage(text)}
+        />
+
+        <TouchableOpacity
+          style={styles.sendButton}
+          onPress={handleSendMessage}
+        >
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f2f2f2'
   },
+  messages: {
+    flex: 1
+  },
+  messageContainer: {
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 8,
+    maxWidth: '80%',
+    alignSelf: 'flex-end'
+  },
+  senderMessageContainer: {
+    backgroundColor: '#007bff',
+    alignSelf: 'flex-end'
+  },
+  receiverMessageContainer: {
+    backgroundColor: '#e5e5e5',
+    alignSelf: 'flex-start'
+  },
+  message: {
+    fontSize: 16,
+    color: '#fff'
+  },
+  senderMessage: {
+    color: '#fff'
+  },
+  receiverMessage: {
+    color: '#000'
+  },
+  date: {
+    fontSize: 12,
+    marginTop: 8,
+    textAlign: 'right'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    backgroundColor: '#fff',
+    borderRadius: 24,
+    paddingLeft: 16,
+    marginRight: 16
+  },
+  sendButton: {
+    width: 80,
+    height: 48,
+    backgroundColor: '#007bff',
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sendButtonText: {
+    fontSize: 16,
+    color: '#fff'
+  }
 });
-
-export default Chatbox;
