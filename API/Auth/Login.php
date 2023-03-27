f9dd6126c0e5a6c9f0d74bba1d4997675311c11e
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-03-27 02:08:54 +0200

    api

--- API/Auth/Login.php
+++ API/Auth/Login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "../Connection.php";
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
@@ -12,10 +13,19 @@ $password = $obj['password'];
 
 $stmt = $con->prepare("SELECT * FROM users WHERE  `password` = ? AND `email` =? ");
 $stmt->execute(array($password, $email));
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$users = $stmt->fetch(PDO::FETCH_ASSOC);
 $count = $stmt->rowCount();
 if ($count > 0) {
-    echo json_encode(array($users));
+
+    $_SESSION['id'] = $users['id'];
+    $_SESSION['name'] = $users['name'];
+    $_SESSION['email'] = $users['email'];
+    // $user_data = array(
+    //     'id' => $_SESSION['id'],
+    //     'email' => $_SESSION['email'],
+    //     'name' => $_SESSION['name'],
+    // );
     // var_dump($users);
+    echo json_encode($_SESSION);
 } else
     echo json_encode(array("status" => "failed"));
---
+++ API/Auth/Logout.php
@@ -0,0 +1,5 @@
+<?php
+include "../Connection.php";
+
+session_start();
+session_destroy();
