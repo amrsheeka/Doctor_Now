160287f0299f53a312ce6a6780c2117d4031335c
Amr <sheeka26122000@gmail.com>
2023-03-28 19:15:04 +0200

    bhf

--- App.js
+++ App.js
@@ -1,33 +1,32 @@
-import React,{useEffect} from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, AppState } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
-import {StackNavigator,StackNavigator2} from "./components/navigation/StackNavigator";
+import { StackNavigator, StackNavigator2 } from "./components/navigation/StackNavigator";
 import CurrentUser from "./components/consts/CurrentUser";
 import { getDoctors } from "./database/Doctors";
 import { getCurrentUser } from "./database/Users";
 
 export default function App() {
   useEffect(() => {
-    CurrentUser.user= getCurrentUser();
+    CurrentUser.user = getCurrentUser();
     getDoctors();
   });
-  
-  
+
   if (Object.keys(CurrentUser.user).length == 0
   ) return (
-    
+
     <NavigationContainer>
       <StackNavigator />
     </NavigationContainer>
   );
-  else 
-  return (
-    
-    <NavigationContainer>
-      <StackNavigator2 />
-    </NavigationContainer>
-  );
+  else
+    return (
+
+      <NavigationContainer>
+        <StackNavigator2 />
+      </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
