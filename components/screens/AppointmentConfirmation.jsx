e4dbdc4f8e652f0c9e1aab6d645ed02ccbfa7bfe
Amr <sheeka26122000@gmail.com>
2023-03-30 00:42:21 +0200

    nk

--- components/screens/AppointmentConfirmation.jsx
+++ components/screens/AppointmentConfirmation.jsx
@@ -6,7 +6,7 @@ const AppointmentConfirmation = ({ navigation, route }) => {
     let Days = ["Monday", "Friday", "Sunday", "Tuesday", "Wednesday", "Thursday", "Saturday"]
     let item = route.params.doctor;
     let image = item.image;
-    const renderDays = ({item}) => (
+    const renderDays = ({ item }) => (
         <TouchableOpacity>
             <Text style={{ paddingHorizontal: 10, fontSize: 20, borderWidth: 1, borderRadius: 3, height: "20%", marginHorizontal: 10 }} >
                 {item}
@@ -15,15 +15,15 @@ const AppointmentConfirmation = ({ navigation, route }) => {
     // let item = { id: 1, name: 'Dr. John Doe', photo: require('../assets/Herbal_Medicine_Male_Avatar.png') }
     return (
         <View style={styles.container}>
-        <View style={styles.header1}>
-        <Text style={styles.heading}>Appointment</Text>
+            <View style={styles.header1}>
+                <Text style={styles.heading}>Appointment</Text>
 
             </View>
             <View style={styles.header}>
-            <Image source={image?{uri: image}:require("../assets/Herbal_Medicine_Male_Avatar.png")} style={styles.cardPhoto} />
+                <Image source={image ? { uri: image } : require("../assets/Herbal_Medicine_Male_Avatar.png")} style={styles.cardPhoto} />
                 <View>
-                <Text style={styles.text1}> {item.name}</Text>
-                <Text style={styles.text2}> {item.describtion}</Text>
+                    <Text style={styles.text1}> {item.name}</Text>
+                    <Text style={styles.text2}> {item.describtion}</Text>
                 </View>
             </View>
             <View style={styles.body}>
@@ -46,7 +46,7 @@ const AppointmentConfirmation = ({ navigation, route }) => {
             <View style={styles.footer}>
                 <View style={{ flex: 2 }}>
                     <Text style={styles.text3}>Biography  </Text>
-                    
+
                     <ScrollView  >
                         <Text style={styles.inp} numberOfLines={10}>
                             Doctors, also known as physicians,
@@ -63,37 +63,37 @@ const AppointmentConfirmation = ({ navigation, route }) => {
                     </Text>
                 </View>
                 <View style={styles.body}>
-                <View style={styles.box}>
-                    <Text style={styles.text}>10:00 PM</Text>
-                </View>
-                <View style={styles.box}>
-                    <Text style={styles.text}>8:00 PM</Text>
-                </View>
-                <View style={styles.box}>
-                    <Text style={styles.text}>6:00 PM</Text>
-                </View>
+                    <View style={styles.box}>
+                        <Text style={styles.text}>10:00 PM</Text>
+                    </View>
+                    <View style={styles.box}>
+                        <Text style={styles.text}>8:00 PM</Text>
+                    </View>
+                    <View style={styles.box}>
+                        <Text style={styles.text}>6:00 PM</Text>
+                    </View>
                 </View>
                 <View style={{ flex: 3 }}>
                     <Text style={styles.text3}>
                         Working Days
                     </Text>
                     <View style={styles.body}>
-                <View style={styles.box1}>
-                    <Text style={styles.text4}>Sun</Text>
-                    <Text style={styles.text4}>20</Text>
+                        <View style={styles.box1}>
+                            <Text style={styles.text4}>Sun</Text>
+                            <Text style={styles.text4}>20</Text>
 
-                </View>
-                <View style={styles.box1}>
-                    <Text style={styles.text4}>Wen</Text>
-                    <Text style={styles.text4}>13</Text>
+                        </View>
+                        <View style={styles.box1}>
+                            <Text style={styles.text4}>Wen</Text>
+                            <Text style={styles.text4}>13</Text>
 
-                </View>
-                <View style={styles.box1}>
-                    <Text style={styles.text4}>Mon</Text>
-                    <Text style={styles.text4}>5</Text>
+                        </View>
+                        <View style={styles.box1}>
+                            <Text style={styles.text4}>Mon</Text>
+                            <Text style={styles.text4}>5</Text>
 
-                </View>
-                </View>
+                        </View>
+                    </View>
                     {/* <FlatList
                         data={Days}
                         renderItem={renderDays}
@@ -112,16 +112,16 @@ const AppointmentConfirmation = ({ navigation, route }) => {
                     >
                     </Button>
                     </View></View> */}
-                <View style={{ flex: 1, flexDirection: "row", marginHorizontal: 10 }}>
+                <View style={{ flexDirection: "row", marginHorizontal: 10, height: "17%" }}>
                     <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Chatbox")}>
-                    <Ionicons name="ios-chatbubble-ellipses-outline" size={40} color="white" />
+                        <Ionicons name="ios-chatbubble-ellipses-outline" size={30} color="white" />
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.button1} onPress={() => navigation.navigate("Details_user_to_appointment", { item })}>
-                    <Text style={styles.buttonText}>Book Appointment</Text>
-                </TouchableOpacity>
+                        <Text style={styles.buttonText}>Book Appointment</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
-</View>
+        </View>
     );
 };
 
@@ -129,25 +129,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-        backgroundColor:"#ffffff"
+        backgroundColor: "#ffffff"
     },
     header: {
         flex: 2,
-        backgroundColor:"#ffffff",
+        backgroundColor: "#ffffff",
         flexDirection: "row",
         // marginVertical: 6,
-        margin:5,
-        padding:10
+        margin: 5,
+        padding: 10
     },
     header1: {
         marginBottom: 10,
-        paddingTop:20
+        paddingTop: 20
     },
     heading: {
         fontSize: 24,
         fontWeight: "bold",
         color: "black",
-        textAlign:"center"
+        textAlign: "center"
     },
     body: {
         flex: 1,
@@ -157,7 +157,7 @@ const styles = StyleSheet.create({
     footer: {
         flex: 5,
         borderRadius: 10,
-        backgroundColor:"#ffffff",
+        backgroundColor: "#ffffff",
         shadowRadius: 2,
         marginHorizontal: 4,
         marginVertical: 6
@@ -165,93 +165,92 @@ const styles = StyleSheet.create({
     text: {
         paddingLeft: 10,
         fontSize: 18,
-        textAlign:"center"
+        textAlign: "center"
     },
     text1: {
         paddingLeft: 5,
         fontSize: 18,
-        fontWeight:"bold",
+        fontWeight: "bold",
     },
     text3: {
         paddingLeft: 5,
         fontSize: 18,
-        fontWeight:"bold",
+        fontWeight: "bold",
         // padding:10
     },
     text2: {
         paddingLeft: 5,
         fontSize: 16,
         // fontWeight:"bold",
-        textAlign:"justify",
-        
+        textAlign: "justify",
+
     },
     text4: {
         paddingLeft: 10,
         fontSize: 18,
-        textAlign:"center"
+        textAlign: "center"
     },
     cardPhoto: {
         width: "40%",
         height: "80%",
-        borderRadius:10
+        borderRadius: 10
 
     },
     box: {
         height: "80%",
         width: "32%",
         borderWidth: 3,
-        borderColor:"#efefef",
-        borderRadius:10,
-        backgroundColor:"#ffffff",
+        borderColor: "#efefef",
+        borderRadius: 10,
+        backgroundColor: "#ffffff",
     },
     box1: {
         height: "60%",
         width: "32%",
         borderWidth: 3,
-        borderColor:"#efefef",
-        borderRadius:10,
-        backgroundColor:"#ffffff",
+        borderColor: "#efefef",
+        borderRadius: 10,
+        backgroundColor: "#ffffff",
     },
     titleText: {
         fontSize: 20,
-        paddingBottom: 7
+        //paddingBottom: 7
     },
-    button:{
+    button: {
         fontSize: 18,
-        paddingHorizontal:"5%",
+        paddingHorizontal: "5%",
         borderWidth: 2,
-        borderRadius:100,
+        borderRadius: 100,
         borderColor: "#ffffff",
-        backgroundColor:"#288771",
+        backgroundColor: "#288771",
         justifyContent: "center",
-        height:"100%",
-        width:"25%"
+        height: "100%",
+        width: "20%"
     },
-    inp:{
+    inp: {
         borderRadius: 10,
-        borderColor:"#efefef",
-        borderWidth: 3, 
-        fontSize: 15 ,
-        padding:10,
-        width:"100%",
-        backgroundColor:"#efefef",
+        borderColor: "#efefef",
+        borderWidth: 3,
+        fontSize: 15,
+        padding: 10,
+        width: "100%",
+        backgroundColor: "#efefef",
 
     },
-    button1:{
+    button1: {
         fontSize: 18,
-        // paddingHorizontal:"20%",
         borderWidth: 2,
-        borderRadius:20,
+        borderRadius: 20,
         borderColor: "#ffffff",
-        backgroundColor:"#288771",
+        backgroundColor: "#288771",
         justifyContent: "center",
-        height:"100%",
-        width:"80%",
-        left:10
+        height: "100%",
+        width: "80%",
+        left: 10
     },
-    buttonText:{
-        textAlign:"center",
-    color:"white"
+    buttonText: {
+        textAlign: "center",
+        color: "white"
     }
 });
 
--- database/Doctors.js
+++ database/Doctors.js
@@ -2,17 +2,17 @@ import Doctor from "../components/consts/Doctor";
 import axios from "axios";
 import Ip from "./Ip";
 const getDoctors = async () => {
-  await axios({
-    method: "get",
-    url: `${Ip.ip}/API/doctors/select.php`,
-    header: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-  }).then((response) => {
-    Doctor.doctors = response.data;
-    //console.log(Doctor.doctors);
-  });
+  // await axios({
+  //   method: "get",
+  //   url: `${Ip.ip}/API/doctors/select.php`,
+  //   header: {
+  //     Accept: "application/json",
+  //     "Content-type": "application/json",
+  //   },
+  // }).then((response) => {
+  //   Doctor.doctors = response.data;
+  //   //console.log(Doctor.doctors);
+  // });
   // Invoking get method to perform a GET request
 
   axios.get(`${Ip.ip}/API/doctors/select.php`).then((response) => {
