b8461fbcfd62da387feed07d69c81f39aff8eb18
Amr <sheeka26122000@gmail.com>
2023-04-01 19:12:26 +0200

    add doc init

--- components/admin/AdminHome.jsx
+++ components/admin/AdminHome.jsx
@@ -1,145 +1,140 @@
 import { all } from "axios";
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Button,
-    TextInput,
-    Image,
-  } from "react-native";
-  import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
-  import { logout } from "../../database/Users";
-  export default function AdminHome({ navigation }) {
-    return (
-      <View style={styles.filterCards}>
-            <TouchableOpacity
-              style={styles.filterCard1}
-              onPress={() => {
-                navigation.navigate("AddDoctor");
-              }}
-            >
-              <View style={styles.filterCardElements}>
-                <View style={styles.cardsIcons}>
-                  <MaterialCommunityIcons
-                    name="stomach"
-                    size={70}
-                    color="white"
-                  />
-                </View>
-                <View style={styles.filterCard1TextVeiw}>
-                  <Text style={styles.filterCard1Text}>Add Doctor</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.filterCard2}
-              onPress={() => {
-                navigation.navigate("AllDoctors",{all:"all"});
-              }}
-            >
-              <View style={styles.filterCardElements}>
-                <View style={styles.cardsIcons}>
-                  <FontAwesome5 name="tooth" size={60} color="white" />
-                </View>
-                <View style={styles.filterCard2TextVeiw}>
-                  <Text style={styles.filterCard2Text}>Edit Doctors</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.filterCard3}
-              onPress={() => {
-                navigation.navigate("AppointmentList");
-              }}
-            >
-              <View style={styles.filterCardElements}>
-                <View style={styles.cardsIcons}>
-                  <Image
-                    source={require("../assets/surgery.png")}
-                    style={{ height: 65, width: 65 }}
-                  />
-                </View>
-                <View style={styles.filterCard3TextVeiw}>
-                  <Text style={styles.filterCard3Text}>AppointmentList</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.filterCard3}
-              onPress={() => {
-                logout().then(()=>{navigation.navigate("StackNavigator")});
-              }}
-            >
-              <View style={styles.filterCardElements}>
-                <View style={styles.cardsIcons}>
-                  <Image
-                    source={require("../assets/surgery.png")}
-                    style={{ height: 65, width: 65 }}
-                  />
-                </View>
-                <View style={styles.filterCard3TextVeiw}>
-                  <Text style={styles.filterCard3Text}>Log Out</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-          </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    filterCards: {
-      flexDirection: "row",
-      height: 140,
-      marginHorizontal: "5%",
-      justifyContent: "center",
-      gap: 10,
-    },
-    filterCard1Text: {
-      color: "white",
-      fontWeight: "900",
-    },
-    filterCard1: {
-      flex: 1,
-      borderRadius: 20,
-      backgroundColor: "#439bdd",
-      height: "100%",
-    },
-    filterCard2Text: {
-      color: "white",
-      fontWeight: "900",
-    },
-    filterCard2: {
-      flex: 1,
-      borderRadius: 20,
-      backgroundColor: "#2bd2fc",
-      height: "100%",
-    },
-    filterCard3Text: {
-      color: "white",
-      fontWeight: "900",
-    },
-    filterCard3: {
-      flex: 1,
-      borderRadius: 20,
-      backgroundColor: "#289ba4",
-      height: "100%",
-    },
-    filterCardElements: {
-      flex: 1,
-      alignItems: "center",
-    },
-    cardsIcons: {
-      flex: 2,
-    },
-    filterCard1TextVeiw: {
-      flex: 1,
-    },
-    filterCard2TextVeiw: {
-      flex: 1,
-    },
-    filterCard3TextVeiw: {
-      flex: 1,
-    },
-  });
-  
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  TextInput,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+export default function AdminHome({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [emailErr, setEmailErr] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  return (
+    <View style={styles.container}>
+      <View style={styles.icon}>
+        <Image source={require("../assets/splash.png")} />
+      </View>
+      <View >
+        <Text style={styles.greeting}>Add a new doctor!</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={{ fontSize: 17, fontWeight: "bold", marginTop: 5 }} >Email</Text>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Text style={{ color: "red" }}>{emailErr}</Text>
+        <Text style={{ fontSize: 17, fontWeight: "bold", marginTop: 5 }} >Password</Text>
+
+        <TextInput
+          placeholder="Password"
+
+          style={styles.input}
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Text style={{ color: "red" }}>{passwordErr}</Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {}}
+
+        >
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+        
+      </View>
+    </View>
+  );
+
+
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  greeting: {
+    fontSize: 28,
+    fontWeight: "bold",
+    fontFamily: "",
+  },
+  inputContainer: {
+    width: "80%",
+
+  },
+  input: {
+    backgroundColor: "#eceff1",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 10,
+
+  },
+  buttonContainer: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+
+  },
+  button: {
+    width: "80%",
+    height: "23%",
+    borderRadius: 20,
+    alignItems: "center",
+    backgroundColor: "#288771",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonText: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+    color: "white",
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  textContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  text: {
+    fontSize: 15,
+    color: "#288771",
+  },
+  icon: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+});
