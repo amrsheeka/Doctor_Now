e31ee36eb9884aa4bbcb5503a35175bb9442eb05
A7med31fimo <ahmedfahim5435644@gmail.com>
2023-04-14 20:25:20 +0200

    ..

--- API/appointment.sql
+++ API/appointment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2023 at 11:46 PM
+-- Generation Time: Apr 14, 2023 at 06:02 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -26,6 +26,7 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `appointment`
 --
+
 CREATE TABLE `appointment` (
   `users_id` int(11) NOT NULL,
   `doctor_id` int(11) NOT NULL,
@@ -35,6 +36,9 @@ CREATE TABLE `appointment` (
   `age` varchar(10) NOT NULL,
   `gender` enum('male','female') NOT NULL,
   `notes` varchar(600) NOT NULL,
+  `doc_name` varchar(30) NOT NULL,
+  `doc_image` varchar(255) DEFAULT NULL,
+  `specialization1` varchar(100) NOT NULL,
   `date_now` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -42,19 +46,8 @@ CREATE TABLE `appointment` (
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`users_id`, `doctor_id`, `date`, `time`, `name_patient`, `age`, `gender`, `notes`, `date_now`) VALUES
-(22, 22, '2023-03-26', '22:27:36', '', '', '', '', '2023-04-07'),
-(22, 22, '0000-00-00', '11:05:00', '', '', '', '', '2023-04-07'),
-(22, 22, '2020-03-11', '11:05:00', '', '', '', '', '2023-04-07'),
-(183, 509, 'Sat Apr 22 2023', '5:30 PM', 'Ahmed fahim', '23', 'male', 'Gggg', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', 'ahmed fhaim', '25+', 'male', 'All is well', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07'),
-(184, 509, 'Fri Apr 07 2023', '5:30 PM', '', '25+', 'male', '', '2023-04-07');
+INSERT INTO `appointment` (`users_id`, `doctor_id`, `date`, `time`, `name_patient`, `age`, `gender`, `notes`, `doc_name`, `doc_image`, `specialization1`, `date_now`) VALUES
+(183, 509, 'Fri Apr 14 2023', '5:30 PM', 'Ahmed', '25+', 'male', 'Ahmed', 'Tamer Yehia HCC', 'https://cdn-dr-images.vezeeta.com/Assets/Images/SelfServiceDoctors/ENTae5e6006161765d0/Profile/150/doctor-hcc-hamer-yehia-orthopedic-and-rheumatology_20200429112418383.jpg', 'Adult Orthopedic Surgery', '2023-04-14');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
