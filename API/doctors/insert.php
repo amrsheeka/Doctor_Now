25a457b7e512bbbc3e76412ab3fcb2c8644c4867
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-04-23 04:43:33 +0200

    .

--- API/doctors/insert.php
+++ API/doctors/insert.php
@@ -22,6 +22,10 @@ $specialization2 = $obj['specialization2'];
 $specialization3 = $obj['specialization3'];
 $address = $obj['address'];
 $price = $obj['price'];
+$password = $obj['password'];
+$gender = $obj['gender'];
+$age = $obj['age'];
+$phone = $obj['phone'];
 // $currency = $obj['currency'];
 // $number = $obj['number'];
 $start = $obj['start'];
@@ -30,9 +34,12 @@ $end = $obj['end'];
 // $status = $obj['status'];
 $x_coordnate = $obj['x_coordnate'];
 $y_coordnate = $obj['y_coordnate'];
-
+$stmt1 = $con->prepare("INSERT INTO `users`(`email`,`name`,`password`,`phone`,`address`,`gender`,`age`,`image`,`is_doctor`,created_at ) VALUES (?,?,?,?,?,?,?,'yes',NOW())");
+$stmt1->execute(array($email,   $name, $password, $phone, $address, $gender, $age, $image));
 $stmt = $con->prepare("INSERT INTO `doctors`(`email`,`title`, `image`, `name`, `title1`, `describtion`, `charactristic`, `charactristic_2`, `specialization`, `specialization1`, `specialization2`, `specialization3`, `address`, `price`, `start`, `end`,`x_coordnate`,`y_coordnate`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 $stmt->execute(array($email, $title, $image, $name, $title1, $describtion, $charactristic, $charactristic_2, $specialization, $specialization1, $specialization2, $specialization3, $address, $price, $start, $end, $x_coordnate, $y_coordnate));
+
+
 $count = $stmt->rowCount();
 if ($count > 0) {
     echo json_encode(array("status" => "success"));
