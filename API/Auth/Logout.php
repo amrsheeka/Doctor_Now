372ad1d5c135cd00a32b92612b00ea80ea82f8ac
Amr <sheeka26122000@gmail.com>
2023-06-20 07:03:27 +0300

    ok

--- API/Auth/Logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../Connection.php";
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With,Content-Type");
-session_start();
-session_destroy();
--- API/function.php
+++
@@ -1,8 +0,0 @@
-<?php
-function filterRequest($requestname)
-{
-    if (!empty($_POST[$requestname])) {
-        return trim(htmlspecialchars(strip_tags($_POST[$requestname])));
-    }
-}
-//+/c4rZR8y~bt9pk
